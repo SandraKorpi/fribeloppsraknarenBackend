5353972daea1d2b6c0dcf7c9dbf0b4340828f0cf
92sankor <92sankor@gafe.molndal.se>
2025-02-06 10:41:05 +0100

    appP

--- src/main/java/sandrakorpi/csnfribeloppapi/CsNfribeloppApiApplication.java
+++ src/main/java/sandrakorpi/csnfribeloppapi/CsNfribeloppApiApplication.java
@@ -1,5 +1,6 @@
 package sandrakorpi.csnfribeloppapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CsNfribeloppApiApplication {
 
     public static void main(String[] args) {
+        //laddar variablerna som inte är synliga i application.properties.
+        Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
         SpringApplication.run(CsNfribeloppApiApplication.class, args);
     }
 
